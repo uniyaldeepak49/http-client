a4441e57684e9a102f705e8c309aeb819c666780
Deepak Uniyal <uniyaldeepak49@gmail.com>
2022-02-18 13:26:06 +0530

    HttpClient Task

---
+++ classes/Http.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace HttpClient;
+
+class Http
+{
+    /**
+     * var $url
+     */
+    private $url = "";
+
+    /**
+     * var $headers
+     */
+
+    private $headers = [];
+
+    /**
+     * var $options
+     */
+
+    private $options = [];
+
+    /**
+     * var $requestMethod
+     */
+
+    private $requestMethod = "";
+
+    /**
+     * var $payload
+     */
+
+    private $payload = [];
+
+    /**
+     * var $response_headers
+     */
+
+    private $response_headers = [];
+
+    /**
+     * Method to set EndpointURL
+     */
+
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * Method to get Endpoint URL
+     */
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Method to set request method i.e GET, POST etc
+     */
+
+    public function setRequestMethod($requestMethod)
+    {
+        $this->requestMethod = $requestMethod;
+    }
+
+    /**
+     * Method to get request method 
+     */
+
+    public function getRequestMethod()
+    {
+        return $this->requestMethod;
+    }
+
+
+    /**
+     * Method to set Headers
+     */
+
+    public function setHeaders($headers)
+    {
+        $this->headers = $headers;
+    }
+
+    /**
+     * Method to get headers
+     */
+
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+    /**
+     * Method to set payload
+     */
+
+    public function setPayLoad($data)
+    {
+        $this->payload = $data;
+    }
+
+    /**
+     * Method to get payload
+     */
+
+    public function getPayLoad()
+    {
+        return json_encode($this->payload);
+    }
+
+    /**
+     * Method to set options
+     */
+
+    public function setOptions()
+    {
+        $this->options = [
+            'http' => [
+                'header' => $this->getHeaders(),
+                'method' => $this->getRequestMethod(),
+                'content' => $this->getPayLoad()
+            ]
+        ];
+    }
+
+    /**
+     * Method to get options
+     */
+
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * Method to make request to Endpoint
+     */
+
+    public function makeRequest()
+    {
+        $context = stream_context_create($this->getOptions());
+        file_get_contents($this->getUrl(), false, $context);
+        $this->response_headers = $http_response_header;
+    }
+    /**
+     * method to get response code
+     */
+
+    public function getCode()
+    {
+        $explode = explode(" ", $this->response_headers[0]);
+        return $explode[1];
+    }
+
+    public function getResponseHeaders(){
+        return $this->response_headers;
+    }
+}
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+include 'classes/Http.php';
+
+use HttpClient\Http;
+
+$httpObj = new http();
+
+$httpObj->setUrl("https://corednacom.corewebdna.com/assessment-endpoint.php");
+
+$httpObj->setRequestMethod('POST');
+
+$httpObj->setPayLoad([
+    'name' => 'Deepak Uniyal',
+    'email' => 'uniyaldeepak49@gmail.com',
+    'url' => 'https://github.com/uniyaldeepak49/http-client'
+]);
+
+$httpObj->setHeaders([
+    'Content-type: application/json',
+    'Authorization: Bearer d417fcbc-94b7-4357-aff3-536c33364428'
+]);
+
+$httpObj->setOptions();
+$httpObj->makeRequest();
+var_dump($httpObj->getResponseHeaders());
+echo '<pre>';
+print_r($httpObj->getResponseHeaders());
+echo '</pre>';
